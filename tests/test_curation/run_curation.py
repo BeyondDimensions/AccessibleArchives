d6fc43b98c7272c23bb4fbbe34cf63f6cd5c9f42
CherryNate <m.kirsch@quantum-dimensions.de>
2024-09-30 19:38:18 +0200

    Bug fixes

--- tests/test_curation/run_curation.py
+++ tests/test_curation/run_curation.py
@@ -1,14 +1,14 @@
 import os
 from _load_src import SRC_PATH
 from curation.curation import curate_pngs
-
+from tqdm import tqdm
 
 input_dir = "/Users/marvinkirsch/DocumentsOB/ScansnapHome/Curation_Folder"
 original_medium = "Photocopy of Folder from BSTU"
 # pdf_path = "/Users/marvinkirsch/DocumentsOB/ScansnapHome/Curation_Folder"
 output_dir = os.path.abspath(os.path.join(
     __file__, "..", "..", "..", "curated_files"))
-for file_name in os.listdir(input_dir):
+for file_name in tqdm(os.listdir(input_dir)):
     if file_name.endswith('.pdf'):  # check if the file is a PDF
         pdf_path = os.path.join(input_dir, file_name)
         dirname = file_name.strip(".pdf")
