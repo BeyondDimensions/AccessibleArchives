5cffffda9bdea7033ff17a3ccf0e2e249c6f96bf
gloverys <email@template.invalid>
2024-10-02 21:11:08 +0200

    database continue interrupted file loading

--- tests/test_rag.py/test_rag.py
+++ tests/test_rag.py/test_rag.py
@@ -3,4 +3,5 @@ from storage.known_doc_collections import TEST_COLLECTION_PATH
 import os
 from rag import initialize_database
 # initialize_database("/mnt/Storage/curated_files/Transcripts")
-initialize_database(os.path.join(TEST_COLLECTION_PATH, "Transcripts"))
+initialize_database(os.path.join(TEST_COLLECTION_PATH, "Transcripts"), load_files=False)
+initialize_database(os.path.join(TEST_COLLECTION_PATH, "Transcripts"), load_files=True)
