46071b808007e92ae0949c9a3c1fdfd45a1521da
gloverys <email@template.invalid>
2024-10-03 09:55:06 +0200

    test for RAG, debugged issue with database management

--- src/storage/known_doc_collections.py
+++ src/storage/known_doc_collections.py
@@ -4,12 +4,12 @@ import os
 import ipfs_api
 from utils import logger
 SRC_PATH = os.path.abspath(os.path.join(__file__, "..", ".."))
-TEST_COLLECTION_PATH = os.path.abspath(os.path.join(
-    SRC_PATH, "..", "tests", "test_storage", "demo_docs"
-))
 # TEST_COLLECTION_PATH = os.path.abspath(os.path.join(
-#     SRC_PATH, "..", ".data"
+#     SRC_PATH, "..", "tests", "test_storage", "demo_docs"
 # ))
+DOCUMENTS_PATH = os.path.abspath(os.path.join(
+    SRC_PATH, "..", ".data"
+))
 
 
 known_doc_collections: list[DocumentCollection] = []
@@ -19,11 +19,11 @@ known_doc_collections: list[DocumentCollection] = []
 def load_known_docs():
     logger.info("Loading known docs...")
     # TODO: replace with appdata memory of known collections
-    ipfs_api.publish(TEST_COLLECTION_PATH)
+    ipfs_api.publish(DOCUMENTS_PATH)
     known_doc_collections.append(DocumentCollection(
         # "/ipfs/QmZ75y9EkkVEpRKhWZn4Ba2E9yNAxbkqGjEFkKdmCUkL5h"
         # "/mnt/Storage/curated_files"
-        TEST_COLLECTION_PATH
+        DOCUMENTS_PATH
     ))
     logger.success("Loaded known docs!")
     return known_doc_collections
